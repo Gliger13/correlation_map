5c19e9570e7a93d446c78b043725c0108a656f7a
Gliger13 <zanevskiyandrey@gmail.com>
2020-05-18 13:46:35 +0300

    Update to version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='correlation_map',
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
