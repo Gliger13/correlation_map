0b60c53b340ff20440a2fffe48a686b156021e2c
Gliger13 <zanevskiyandrey@gmail.com>
2022-06-10 02:31:56 +0300

    refactor: remove tests
    Remove tests and test framework as they are no applicable and there are no
    plans to continue implementing them.

--- tests/unit-tests/core-tests/images-tests/test_gray_transformation.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from test_framework.asserts.image.image import check_gray_transformation
-from test_framework.tools.common.data_factory import get_test_data
-
-
-@pytest.mark.parametrize('test_data', get_test_data(__file__))
-def test_gray_transformation(test_data, test_source_image, test_expected_image):
-    check_gray_transformation(test_source_image, test_expected_image)
--- tests/unit-tests/core-tests/images-tests/test_rotate_image.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from test_framework.asserts.image.image import check_rotate_image
-from test_framework.tools.common.data_factory import get_test_data
-
-
-@pytest.mark.parametrize('test_data', get_test_data(__file__))
-def test_rotate_image(test_data, test_source_image, test_expected_image):
-    check_rotate_image(test_source_image, test_expected_image)
